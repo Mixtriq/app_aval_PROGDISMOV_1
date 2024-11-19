7b236545d726faf1101242bf1a200dd9bc890784
Carlos Eduardo <mixtrik0@gmail.com>
2024-11-19 16:45:27 -0300

    construção parcial da lista. botei na main pra agilizar

--- filmaiada/lib/main.dart
+++ filmaiada/lib/main.dart
@@ -1,1 +1,37 @@
+import 'package:flutter/material.dart';
+import './data.dart'; // Certifique-se de que o arquivo data.dart contém a lista de filmes.
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text("Filmaiada")),
+        body: ListView(
+          padding: const EdgeInsets.all(8),
+          children: <Widget>[
+            for (var movie in movies)
+              Column(
+                children: [
+                  InkWell(
+                      child: Container(
+                        height: 50,
+                        color: Colors.blue,
+                        child: Center(child: Text(movie.name)),
+                      ),
+                      onTap: () => {}),
+                  const SizedBox(height: 10),
+                ],
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
