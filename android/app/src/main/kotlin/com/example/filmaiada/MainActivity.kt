5cca2e26af57737ff14450c0e01b578b7f3352eb
Kevin Machado <kevinm4ch@gmail.com>
2025-01-13 14:02:20 -0300

    refactor: ajeitando .gitignore

--- android/app/src/main/kotlin/com/example/filmaiada/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.filmaiada
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/utils/routes.dart
+++
@@ -1,7 +0,0 @@
-class AppRoutes {
-  static const home = '/';
-  static const movieInfo = '/movie_info';
-  static const movies = '/movies';
-  static const aboutUs = '/about_us';
-  static const watchList = '/watch_list';
-}
